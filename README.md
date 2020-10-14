043d627863c68f284df236451db2c0ad1f559ff2
t.yaginuma <56906955+t-yaginuma@users.noreply.github.com>
2020-10-14 18:47:07 +0900

    修正

--- README.md
+++ README.md
@@ -8,6 +8,9 @@ jpeg / png / gif / svg
 1. 圧縮したい画像を`./images/src/`に格納。
 1. `gulp`
 1. `./images/dest/`に圧縮された画像が格納される。
+1. `./images/src/`こちらに入る
+1. `./images/dest/`入れたファイルは削除される。
+1. `./images/archive/`に元ファイルのバックアップが取られる。
 
 #### その他機能
 1. `gulp clean` : `./images/dest/`内のファイルを全て削除
--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,38 @@
-const { src, dest } = require("gulp"),
+const { src, dest, series } = require("gulp"),
  imagemin = require('gulp-imagemin'),
  mozjpeg = require('imagemin-mozjpeg'),
  pngquant = require('imagemin-pngquant');
  clean = require('gulp-clean');
 
+const QUALITY_JPEG = 80;
+const QUALITY_PNG = [ 0.65, 0.8 ];
+
+let timeNow = new Date();
+let timeNowYear = timeNow.getFullYear();
+let timeNowMonth = timeNow.getMonth() + 1;
+let timeNowDate = timeNow.getDate();
+let timeNowHours = timeNow.getHours();
+let timeNowMinites = timeNow.getMinutes();
+let timeNowSeconds = timeNow.getSeconds();
+
+let today = `${timeNowYear}${timeNowMonth}${timeNowDate}_${timeNowHours}${timeNowMinites}_${timeNowSeconds}`;
+
 const taskClean = () =>
-  src('images/dest/*', { read: false })
+  src('images/src/*', { read: false })
   .pipe(clean());
+
+const taskArchive = ()=>
+  src('images/src/*')
+  .pipe(dest('images/archive/' + today))
 
 const taskImagemin = () => 
   src('images/src/*')
   .pipe(imagemin([
     pngquant({ 
-    quality: [ 0.65, 0.8 ], speed: 1 
+    quality: QUALITY_PNG, speed: 1 
     }),
     mozjpeg({ 
-    quality: 80
+    quality: QUALITY_JPEG
     }),
     imagemin.gifsicle({
     interlaced: false
@@ -29,5 +46,4 @@ const taskImagemin = () =>
   ]))
   .pipe(dest('images/dest'));
 
-exports.default = taskImagemin;
-exports.clean = taskClean;
+exports.default = series(taskImagemin, taskArchive, taskClean);
